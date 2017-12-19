872ebb7dca1fe4f5b6bd5ea9c46939110560f181
wuyu <2568986399@qq.com>
2017-12-19 11:13:18 +0800

    时间选择器兼容小米5

--- YiCaiGuoJi_BranchV3/app/src/main/java/com/developer/lecai/view/MonPickerDialog.java
+++ YiCaiGuoJi_BranchV3/app/src/main/java/com/developer/lecai/view/MonPickerDialog.java
@@ -2,35 +2,87 @@ package com.developer.lecai.view;
 
 import android.app.DatePickerDialog;
 import android.content.Context;
+import android.content.res.Resources;
+import android.os.Build;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.DatePicker;
+
+import java.lang.reflect.Field;
 
 /**
  * Created by liuwei on 2017/8/29.
  */
 
 public class MonPickerDialog extends DatePickerDialog {
-    public MonPickerDialog(Context context, OnDateSetListener callBack, int year, int monthOfYear, int dayOfMonth) {
-        super(context, callBack, year, monthOfYear, dayOfMonth);
-
-//        this.setTitle(year + "年");
-        ((ViewGroup) ((ViewGroup) this.getDatePicker().getChildAt(0)).getChildAt(0)).getChildAt(0).setVisibility(View.GONE);
-        ((ViewGroup) ((ViewGroup) this.getDatePicker().getChildAt(0)).getChildAt(0)).getChildAt(2).setVisibility(View.GONE);
-
-    }
 
     public MonPickerDialog(Context context, int theme, OnDateSetListener listener, int year, int monthOfYear, int dayOfMonth) {
         super(context, theme, listener, year, monthOfYear, dayOfMonth);
-
-//        this.setTitle(year + "年" );
-        ((ViewGroup) ((ViewGroup) this.getDatePicker().getChildAt(0)).getChildAt(0)).getChildAt(0).setVisibility(View.GONE);
-        ((ViewGroup) ((ViewGroup) this.getDatePicker().getChildAt(0)).getChildAt(0)).getChildAt(2).setVisibility(View.GONE);
+        hidePicker();
     }
 
     @Override
     public void onDateChanged(DatePicker view, int year, int month, int day) {
         super.onDateChanged(view, year, month, day);
         this.setTitle(month + "月");
+    }
+
+    /**
+     * 传入一个DatePicker对象,隐藏或者显示相应的时间项
+     */
+    private void hidePicker() {
+        // 利用java反射技术得到picker内部的属性，并对其进行操作
+        Class<? extends DatePicker> c = getDatePicker().getClass();
+        try {
+            Field fd = null, fm = null, fy = null;
+            // 系统版本大于5.0
+            if (Build.VERSION.SDK_INT >= 5) {
+                int daySpinnerId = Resources.getSystem().getIdentifier("day", "id", "android");
+                int yearSpinnerId = Resources.getSystem().getIdentifier("year", "id", "android");
+                //隐藏日
+                if (daySpinnerId != 0) {
+                    View daySpinner = getDatePicker().findViewById(daySpinnerId);
+                    if (daySpinner != null) {
+                        daySpinner.setVisibility(View.GONE);
+                    }
+                }
+                //隐藏年
+                if (yearSpinnerId != 0) {
+                    View monthSpinner = getDatePicker().findViewById(yearSpinnerId);
+                    if (monthSpinner != null) {
+                        monthSpinner.setVisibility(View.GONE);
+                    }
+                }
+                return;
+            } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+                // 系统版本大于4.0
+                fd = c.getDeclaredField("mDaySpinner");
+                fm = c.getDeclaredField("mMonthSpinner");
+                fy = c.getDeclaredField("mYearSpinner");
+            } else {
+                fd = c.getDeclaredField("mDayPicker");
+                fm = c.getDeclaredField("mMonthPicker");
+                fy = c.getDeclaredField("mYearPicker");
+            }
+            // 对字段获取设置权限
+            fd.setAccessible(true);
+            fm.setAccessible(true);
+            fy.setAccessible(true);
+            // 得到对应的控件
+            View vd = (View) fd.get(getDatePicker());
+            View vm = (View) fm.get(getDatePicker());
+            View vy = (View) fy.get(getDatePicker());
+
+            vd.setVisibility(View.GONE);
+            vy.setVisibility(View.GONE);
+            vm.setVisibility(View.VISIBLE);
+
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
     }
 }
